91c7532da6a74c7a20f2107a6667be0bcbe1f7e1
Ricardo Alberco <sabado17_08_2024@outlook.com>
2026-03-06 17:04:12 -0500

    Agregando try/catch en citaController

--- controllers/CitaController.cs
+++ controllers/CitaController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using TuProyecto.Enums;
 namespace ApiClinica.Controllers
 {
     [ApiController]
@@ -14,15 +15,55 @@ namespace ApiClinica.Controllers
         [HttpGet]
         public IActionResult getCitas()
         {
-            List<Cita> citas = _citaService.obtenerCitas();
-            return Ok(citas);
+            try
+            {
+                List<Cita> citas = _citaService.obtenerCitas();
+                return Ok(citas);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Ocurrio un error interno en el servidor");
+            }
         }
 
         [HttpPost]
         public IActionResult crearCita(CitaCreateDTO dto)
         {
-            Cita cita = _citaService.registrarCita(dto);
-            return Created("", cita);
+            try
+            {
+                Cita cita = _citaService.registrarCita(dto);
+                return Created("", cita);
+            }
+            catch (ArgumentException ex)
+            {
+                return StatusCode(404,ex.Message);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Ocurrio un error interno en el servidor");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult cambiarEstado(int id, EstadoCita estado)
+        {
+            try
+            {
+                if (id <= 0)
+                {
+                    return BadRequest("El id debe ser mayor que 0");
+                }
+                Cita citaActualizada = _citaService.cambiarEstado(id,estado);
+                return Ok(citaActualizada);
+            }
+            catch (ArgumentException ex)
+            {
+                return StatusCode(404,ex.Message);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Ocurrio un error interno en el servidor");
+            }
         }
     }
 }
